6cd0946831cbe0c6cdb2ddaeb0cbc9cb13ad89c4
zalalalyas <zalalalyas@gmail.com>
2021-12-30 04:34:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        body{
-    background-color: red;
-}
+        
+      body {
+        background: #FFFFFF;
+        margin: 0;
+      }
+
+      #backgroundImage {
+        border: none;
+        height: 100%;
+        pointer-events: none;
+        position: fixed;
+        top: 0;
+        visibility: hidden;
+        width: 100%;
+      }
+
+      [show-background-image] #backgroundImage {
+        visibility: visible;
+      }
+    
     </style>
+    
 </head>
 <body>
     <p><b>hellow world</b></p>
